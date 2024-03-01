0350347f05442e392dbc5f54d8ea57ec998cbd1c
Richard Messenger <rchrdmessenger@gmail.com>
2024-03-01 14:22:08 +0000

    Update

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+
+
+  // Datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+
+  // Select Initialization
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+
+  // Collapsible Initialization
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+
+
+  // Initialize modal-trigger buttons
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal, {});
+
+  // Below courtesy of Chat GPT
+
+  let modalTriggerButtons = document.querySelectorAll('.modal-trigger');
+  modalTriggerButtons.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+      event.preventDefault();
+      let target = btn.getAttribute('href');
+      let modalInstance = M.Modal.getInstance(document.querySelector(target));
+      modalInstance.open();
+    });
+  });
+});
